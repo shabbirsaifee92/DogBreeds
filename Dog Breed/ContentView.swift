fbf139232e954ef5e22be1c005d833ac244a3242
shabbirsaifee92 <shabbirsaifee91@gmail.com>
2020-08-01 18:12:21 -0400

    Initial Commit

---
+++ Dog Breed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dog Breed
+//
+//  Created by Shabbir Saifee on 8/1/20.
+//  Copyright © 2020 Shabbir Saifee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
